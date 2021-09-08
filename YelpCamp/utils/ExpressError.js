83c0e63c9c11a685edc42ea1bb9c45171813ebbf
Ray Haddad <rayghaddad@gmail.com>
2021-09-08 13:33:22 +0300

    removed unnecessary files

--- YelpCamp/utils/ExpressError.js
+++
@@ -1,9 +0,0 @@
-class ExpressError extends Error {
-    constructor(message, statusCode) {
-        //calls constructor of Error
-        super();
-        this.message = message;
-        this.statusCode = statusCode;
-    }
-}
-module.exports = ExpressError;
